76ebf8a0d1f99ad41e06e1c8fa0ed7cb3c620e99
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-12 13:15:58 -0400

    Exercise 2 Selection done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "Science":
+                    Console.WriteLine("Science can be interesting at times");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Social Studies is so boring");
+                    break;
+                case "English":
+                    Console.WriteLine("English is too much reading");
+                    break;
+                default: Console.WriteLine("I dont mind that subject");
+                    break;
+            }
         }
     }
 }
